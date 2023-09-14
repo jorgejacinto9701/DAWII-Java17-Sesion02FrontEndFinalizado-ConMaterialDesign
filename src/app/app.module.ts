85b2eaabd763baa7ededf47861bf9bdd96613f67
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-09-13 22:25:30 -0500

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AddModalidadComponent } from './components/add-modalidad/add-modalidad.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app.material.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { AddModalidadComponent } from './components/add-modalidad/add-modalidad.
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
